1de451df5168b5f3dc843aa772c3b585c7b43bfe
Alexandru Martinas <alexandrumartinas33@gmail.com>
2017-12-01 10:13:16 +0100

    Delete StringSimilarity.java

--- src/PD/StringSimilarity.java
+++
@@ -1,9 +0,0 @@
-package PD;
-
-import java.util.List;
-
-public interface StringSimilarity {
-
-    public int countMatches(List<NTuple<String>> nTuples1, 
-            List<NTuple<String>> nTuples2);
-}
