a7be3aa25ec14a36bc115759973ee572914078ba
Alexandru Martinas <alexandrumartinas33@gmail.com>
2017-12-01 10:12:49 +0100

    Changed name

---
+++ src/Validator/StringSimilarity.java
@@ -0,0 +1,9 @@
+package PD;
+
+import java.util.List;
+
+public interface StringSimilarity {
+
+    public int countMatches(List<NTuple<String>> nTuples1, 
+            List<NTuple<String>> nTuples2);
+}
